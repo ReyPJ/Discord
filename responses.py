7a61410e434c845da132fec1bccaeaccb5e77458
Reyner <reyenr1012002@gmail.com>
2024-03-13 00:00:07 -0600

    words

--- responses.py
+++ responses.py
@@ -7,7 +7,47 @@ def get_responses(user_input: str) -> str:
     if lowered == 'gaeso':
         return choice(['Gaeso es puto', 'Mira Gaeso un turco', 'Como te van a hackear la cuenta Gaeso xD'])
 
-    elif lowered == 'benji' or (lowered == 'pae' or lowered == 'mclovin'):
+    elif lowered == 'benji':
+        return choice(
+            [
+                'pendejo',
+                'estupido bastardo',
+                'cara de nalga',
+                'de seguro vives en el cocho',
+                'negro', 'gei lesbiano',
+                'judas',
+                'cabeza de cebolla',
+                'pedo de rata',
+                'no es mi culpa que aigas nacido negro'
+            ])
+    elif lowered == 'pae':
+        return choice(
+            [
+                'pendejo',
+                'estupido bastardo',
+                'cara de nalga',
+                'de seguro vives en el cocho',
+                'negro', 'gei lesbiano',
+                'judas',
+                'cabeza de cebolla',
+                'pedo de rata',
+                'no es mi culpa que aigas nacido negro'
+            ])
+
+    elif lowered == 'benji':
+        return choice(
+            [
+                'pendejo',
+                'estupido bastardo',
+                'cara de nalga',
+                'de seguro vives en el cocho',
+                'negro', 'gei lesbiano',
+                'judas',
+                'cabeza de cebolla',
+                'pedo de rata',
+                'no es mi culpa que aigas nacido negro'
+            ])
+    elif lowered == 'mclovin':
         return choice(
             [
                 'pendejo',
