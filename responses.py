0f1add9840b367222fadd76f61d8714ca9bd9434
Reyner <reyenr1012002@gmail.com>
2024-03-13 00:13:14 -0600

    words

--- responses.py
+++ responses.py
@@ -10,53 +10,43 @@ def get_responses(user_input: str) -> str:
     elif lowered == 'benji':
         return choice(
             [
-                'pendejo',
-                'estupido bastardo',
-                'cara de nalga',
-                'de seguro vives en el cocho',
-                'negro', 'gei lesbiano',
-                'judas',
-                'cabeza de cebolla',
-                'pedo de rata',
-                'no es mi culpa que aigas nacido negro'
+                'Benji eres un pendejo',
+                'Benji eres un estupido bastardo',
+                'Benji tienes cara de nalga',
+                'Benji de seguro vives en el cocho',
+                'Benji eres un negro',
+                'Benji eres un gei lesbiano',
+                'Benji eres  judas',
+                'Benji eres un cabeza de cebolla',
+                'Benji eres un pedo de rata',
+                'Benji no es mi culpa que aigas nacido negro'
             ])
     elif lowered == 'pae':
         return choice(
             [
-                'pendejo',
-                'estupido bastardo',
-                'cara de nalga',
-                'de seguro vives en el cocho',
-                'negro', 'gei lesbiano',
-                'judas',
-                'cabeza de cebolla',
-                'pedo de rata',
-                'no es mi culpa que aigas nacido negro'
+                'Pae eres un pendejo',
+                'Pae eres un estupido bastardo',
+                'Pae tienes cara de nalga',
+                'Pae de seguro vives en el cocho',
+                'Pae eres un negro',
+                'Pae eres un gei lesbiano',
+                'Pae eres  judas',
+                'Pae eres un cabeza de cebolla',
+                'Pae eres un pedo de rata',
+                'Pae no es mi culpa que aigas nacido negro'
             ])
 
-    elif lowered == 'benji':
-        return choice(
-            [
-                'pendejo',
-                'estupido bastardo',
-                'cara de nalga',
-                'de seguro vives en el cocho',
-                'negro', 'gei lesbiano',
-                'judas',
-                'cabeza de cebolla',
-                'pedo de rata',
-                'no es mi culpa que aigas nacido negro'
-            ])
     elif lowered == 'mclovin':
         return choice(
             [
-                'pendejo',
-                'estupido bastardo',
-                'cara de nalga',
-                'de seguro vives en el cocho',
-                'negro', 'gei lesbiano',
-                'judas',
-                'cabeza de cebolla',
-                'pedo de rata',
-                'no es mi culpa que aigas nacido negro'
+                'Mclovin eres un pendejo',
+                'Mclovin eres un estupido bastardo',
+                'Mclovin tienes cara de nalga',
+                'Mclovin de seguro vives en el cocho',
+                'Mclovin eres un negro',
+                'Mclovin eres un gei lesbiano',
+                'Mclovin eres  judas',
+                'Mclovin eres un cabeza de cebolla',
+                'Mclovin eres un pedo de rata',
+                'Mclovin no es mi culpa que aigas nacido negro'
             ])
