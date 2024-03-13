d039bf2fa9a7c4ac7394d8bc85d85fefab60129f
Reyner <reyenr1012002@gmail.com>
2024-03-12 22:22:38 -0600

    third

--- responses.py
+++ responses.py
@@ -1,7 +1,10 @@
 from random import choice
 
-def get_responses(user_input: str) -> str | None:
+
+def get_responses(user_input: str) -> str:
     lowered: str = user_input.lower()
 
     if lowered == 'gaeso':
         return choice(['Gaeso es puto', 'Mira gaeso un turco', 'Como te van a hackear la cuenta xD'])
+    else:
+        pass
