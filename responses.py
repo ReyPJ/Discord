a1d0da5ee7b44c127cbf0d2a1e47c3b456d15193
Reyner <reyenr1012002@gmail.com>
2024-03-12 21:04:17 -0600

    Bot v1

---
+++ responses.py
@@ -0,0 +1,8 @@
+from random import choice
+
+def get_responses(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == 'gaeso':
+        return choice(['Gaeso es puto', 'Mira gaeso un turco', 'Como te van a hackear la cuenta xD'])
+
