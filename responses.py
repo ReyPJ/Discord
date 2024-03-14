d3eda0da978ba61f65bd9d3c7b442b4095893fcd
Reyner <reyenr1012002@gmail.com>
2024-03-13 19:52:03 -0600

    commited

--- responses.py
+++ responses.py
@@ -1,56 +1,56 @@
 from random import choice
-from urllib import response
 
 
 def get_responses(user_input: str) -> str:
-    lowered: str = user_input.lower()
+    name_responses = {
+        'gaeso': [
+            'Gaeso es puto',
+            'Mira Gaeso un turco',
+            'Como te van a hackear la cuenta Gaeso xD'
+        ],
+        'benji': [
+            'Benji eres un pendejo',
+            'Benji eres un estupido bastardo',
+            'Benji tienes cara de nalga',
+            'Benji de seguro vives en el cocho',
+            'Benji eres un negro',
+            'Benji eres un gei lesbiano',
+            'Benji eres  judas',
+            'Benji eres un cabeza de cebolla',
+            'Benji eres un pedo de rata',
+            'Benji no es mi culpa que aigas nacido negro'
+        ],
+        'pae': [
+            'Pae eres un pendejo',
+            'Pae eres un estupido bastardo',
+            'Pae tienes cara de nalga',
+            'Pae de seguro vives en el cocho',
+            'Pae eres un negro',
+            'Pae eres un gei lesbiano',
+            'Pae eres  judas',
+            'Pae eres un cabeza de cebolla',
+            'Pae eres un pedo de rata',
+            'Pae no es mi culpa que aigas nacido negro'
+        ],
+        'mclovin': [
+            'Mclovin eres un pendejo',
+            'Mclovin eres un estupido bastardo',
+            'Mclovin tienes cara de nalga',
+            'Mclovin de seguro vives en el cocho',
+            'Mclovin eres un negro',
+            'Mclovin eres un gei lesbiano',
+            'Mclovin eres  judas',
+            'Mclovin eres un cabeza de cebolla',
+            'Mclovin eres un pedo de rata',
+            'Mclovin no es mi culpa que aigas nacido negro'
+        ],
+        'runas': [
+            'https://www.deeplol.gg/champions?version=14.5&tier=Challenger+'
+        ]
+    }
 
-    if lowered == 'gaeso':
-        return choice(['Gaeso es puto', 'Mira Gaeso un turco', 'Como te van a hackear la cuenta Gaeso xD'])
-
-    elif lowered == 'benji':
-        return choice(
-            [
-                'Benji eres un pendejo',
-                'Benji eres un estupido bastardo',
-                'Benji tienes cara de nalga',
-                'Benji de seguro vives en el cocho',
-                'Benji eres un negro',
-                'Benji eres un gei lesbiano',
-                'Benji eres  judas',
-                'Benji eres un cabeza de cebolla',
-                'Benji eres un pedo de rata',
-                'Benji no es mi culpa que aigas nacido negro'
-            ])
-    elif lowered == 'pae':
-        return choice(
-            [
-                'Pae eres un pendejo',
-                'Pae eres un estupido bastardo',
-                'Pae tienes cara de nalga',
-                'Pae de seguro vives en el cocho',
-                'Pae eres un negro',
-                'Pae eres un gei lesbiano',
-                'Pae eres  judas',
-                'Pae eres un cabeza de cebolla',
-                'Pae eres un pedo de rata',
-                'Pae no es mi culpa que aigas nacido negro'
-            ])
-
-    elif lowered == 'mclovin':
-        return choice(
-            [
-                'Mclovin eres un pendejo',
-                'Mclovin eres un estupido bastardo',
-                'Mclovin tienes cara de nalga',
-                'Mclovin de seguro vives en el cocho',
-                'Mclovin eres un negro',
-                'Mclovin eres un gei lesbiano',
-                'Mclovin eres  judas',
-                'Mclovin eres un cabeza de cebolla',
-                'Mclovin eres un pedo de rata',
-                'Mclovin no es mi culpa que aigas nacido negro'
-            ])
-
-    elif lowered == 'runas':
-        return 'https://www.deeplol.gg/champions?version=14.5&tier=Challenger+'
+    lowered = user_input.lower()
+    if lowered in name_responses:
+        return choice(name_responses[lowered])
+    else:
+        return 'Callate'
